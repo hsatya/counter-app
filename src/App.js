189d96069445ff6020e1135bf8f533278e131119
Harshit Satya <harshitsatya5@gmail.com>
2021-12-17 17:28:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UseReducer from './components/hooks/UseReducer';
+
+
+const App = () => {
+  return (
+    <>
+      <UseReducer />
+    </>
+  )
+}
+
+
+export default App;
